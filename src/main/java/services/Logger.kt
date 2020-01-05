0d5d35f88cfc575164fd1f3ee66710b5ff81f892
fan-mag <fan-mag@mail.ru>
2020-01-06 02:04:16 +0300

    1.0.1e Logger threading

--- src/main/java/services/Logger.kt
+++ src/main/java/services/Logger.kt
@@ -2,13 +2,12 @@ package services
 
 import helpers.BodyBuilder
 import io.restassured.RestAssured.given
+import java.util.concurrent.Executors
 
 class Logger(val service: String?, val message: String) : Runnable {
 
     fun log() {
-        synchronized(this) {
-            Thread(this).start()
-        }
+        threadQueue.submit(Thread(this))
     }
 
     override fun run() {
@@ -23,6 +22,7 @@ class Logger(val service: String?, val message: String) : Runnable {
 
     companion object {
         lateinit var URL: String
+        val threadQueue = Executors.newFixedThreadPool(1)
         fun log(service: String? = null, message: String) {
             Logger(service, message).log()
         }
